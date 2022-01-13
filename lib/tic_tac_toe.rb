bbe22218d3375f7a19dfbfef6ae3c1d382ad3f9d
Logravia <j.alunans@gmail.com>
2022-01-13 19:17:51 +0200

    Move, update GameManager methods related to board to a class Board

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,110 +10,17 @@ end
 class GameManager
   # 2D array for 3x3 board
   attr_reader :board
-
   def initialize
-    @board = Array.new(3) { Array.new(3) }
-    @players = []
-    @score = []
-    @screen = nil
+    @board = Board.new()
   end
-
-  def board_state?
-    # TODO: refactor checks
-    # horizontal check
-    @board.each do |row|
-      return 'victory' if n_consecutive?(row[0], row, 3)
-    end
-
-    # vertical check
-    column = 0
-    while column < 3
-      row = 0
-      token = @board[row][column]
-      token_count = 0
-      while row < 3
-        break if @board[row][column].nil?
-
-        token_count += 1 if @board[row][column] == token
-        # go down the row
-        row += 1
-      end
-      return 'victory' if token_count == 3
-
-      # finished one column, moving on to next
-      column += 1
-    end
-
-    # main diagonal check
-    column = 0
-    row = 0
-    token = @board[0][0]
-    token_count = 0
-    while column < 3
-      break if @board[row][column].nil?
-
-      token_count += 1 if @board[row][column] == token
-      # go diagonally to next part
-      row += 1
-      column += 1
-    end
-    return 'victory' if token_count == 3
-
-    # reverse diagonal check
-    column = 0
-    row = 2
-    token = @board[row][column]
-    token_count = 0
-    while column < 3
-      break if @board[row][column].nil?
-
-      token_count += 1 if @board[row][column] == token
-      # go diagonally to next part
-      row -= 1
-      column += 1
-    end
-    return 'victory' if token_count == 3
-
-    # none of the victory conditions were achieved therefore the board can be unfinished or tied
-    # while there is a nil, there's a place to make a move, board can't be finished
-    @board.flatten.include?(nil) ? 'unfinished' : 'tie'
-  end
-
-  # returns true when elements are consecutively in a row n times
-  def n_consecutive?(token, row, num)
-    token_count = 0
-    row.each do |cell_token|
-      break if cell_token.nil?
-
-      if token == cell_token
-        token_count += 1
-      else
-        token_count = 0
-      end
-    end
-    token_count == num
-  end
-
   def play
     loop do
       # Main game loop
     end
   end
 
-  def next_round
-    # Move player's to round n
-  end
-
-  def read_move(player)
-    # Get a move from the player
-    # Check its validity
-  end
-
-  def write_move(where, player); end
-
   def reset
-    @board = Array.new(3) { Array.new(3) }
-    @screen.clear
+    @board = Board.new()
   end
 end
 
@@ -126,23 +33,13 @@ class Screen
     @cross = { [0, 2] => '#', [0, 6] => '#', [1, 4] => '#', [2, 2] => '#', [2, 6] => '#' }
   end
 
-  def print_board(_board, _size)
-    width = 8
-    height = 3
-    vertical_line(width, height)
-    horizontal_line(width)
-    vertical_line(width, height)
-    horizontal_line(width)
-    vertical_line(width, height)
-  end
-
-  def gen_display_board(_width = 8, _height = 3)
+  def gen_display_board(height)
     arr = []
-    arr = gen_vertical_line(arr)
-    arr = gen_horizontal_line(arr)
-    arr = gen_vertical_line(arr)
-    arr = gen_horizontal_line(arr)
-    arr = gen_vertical_line(arr)
+    height.time do
+      arr = gen_horizontal_line(arr)
+      arr = gen_vertical_line(arr)
+      arr = gen_horizontal_line(arr)
+    end
   end
 
   def board_arr_to_hash(arr)
@@ -155,18 +52,20 @@ class Screen
     hash
   end
 
-  def gen_vertical_line(arr, width = 8, height = 3)
+  def gen_vertical_line(arr)
+    spacing = 8
     height.times do
       line = []
-      width.times do
+      line << '|'
+      spacing.times do
         line << ' '
       end
       line << '|'
-      width.times do
+      spacing.times do
         line << ' '
       end
       line << '|'
-      width.times do
+      spacing.times do
         line << ' '
       end
       line << "\n"
@@ -175,22 +74,24 @@ class Screen
     arr
   end
 
-  def gen_horizontal_line(arr, width = 8)
+  def gen_horizontal_line(arr)
+    spacing = 8
     line = []
-    width.times do
+    spacing.times do
       line << '-'
     end
     line << '+'
-    width.times do
+    spacing.times do
       line << '-'
     end
     line << '+'
-    width.times do
+    spacing.times do
       line << '-'
     end
     line << "\n"
     arr << line
   end
+
   def print_symbol(arr)
     arr.each do |row|
       row.each do |v|
@@ -203,11 +104,13 @@ class Screen
       puts('')
     end
   end
+
   def print_hash_canvas(hash)
     hash.each do |_k, v|
       print v
     end
   end
+
   def put_symbol(hash, x, y)
     y_offset = 4 * y
     x_offset = 8 * x
@@ -215,6 +118,7 @@ class Screen
       hash[[k[0] + y_offset, k[1] + x_offset]] = v
     end
   end
+
   def print_prompt; end
   def print_result; end
   def clear; end
@@ -224,19 +128,134 @@ end
 class Player
   attr_accessor :symbol, :score
 
-  def initialize(name, symbol)
+  def initialize(name)
     @name = name
-    @symbol = symbol
     @score = 0
   end
 
-  def choose(choice)
-    # get a choice
-    # return it
+  def choose
+    chosen_square = gets
   end
 end
 
-screen = Screen.new
-canvas_arr = screen.gen_display_board
-hash = screen.board_arr_to_hash(canvas_arr)
-screen.print_hash_canvas(hash)
+class Board
+  attr_reader :board, :size
+
+  def initialize(size = 3, tokens_to_win = 3)
+    @board = Array.new(size) { Array.new(size) }
+    @board_to_draw = []
+    @size = size
+    @tokens_to_win = tokens_to_win
+  end
+  def set_token(who, where)
+
+    return false if where > board.length * board[0].length
+    # track on which element we are on
+    cur_element_num = 1
+    board.each_with_index do |row, y|
+      row.each_with_index do |val, x|
+        if where == cur_element_num
+          if val.nil?
+            board[y][x] = who
+            return true
+          else # place already has a value
+            return false
+          end
+        end
+        # checked one element time to update the count
+        cur_element_num += 1
+      end
+    end
+  end
+
+  # Returns three possible states of the board: tie, unfinished, victory
+  def state?
+    if victory?
+      'victory'
+    elsif board.flatten.any?(nil)
+      'unfinished'
+    else
+      'tie'
+    end
+  end
+
+  private
+
+  attr_reader :tokens_to_win
+
+  # Gets columns of the board
+  def columns
+    column = []
+    arr_of_columns = []
+    cur_col = 0
+    while cur_col < board[0].length
+      cur_row = 0
+      while cur_row < board.length
+        column << board[cur_row][cur_col]
+        cur_row += 1
+      end
+      cur_col += 1
+      arr_of_columns << column
+      column = []
+    end
+    arr_of_columns
+  end
+
+  # Gets left or right diagonals of the board
+  def l_diagonals(board = self.board)
+    start_of_diagonal = 0
+    diagonal = []
+    arr_of_diagonals = []
+    while start_of_diagonal < board[0].length
+      cur_col = start_of_diagonal
+      cur_row = 0
+      while cur_row < board.length && cur_col < board[0].length
+        diagonal << board[cur_row][cur_col]
+        cur_row += 1
+        cur_col += 1
+      end
+      arr_of_diagonals << diagonal
+      diagonal = []
+      start_of_diagonal += 1
+    end
+    arr_of_diagonals
+  end
+
+  def r_diagonals
+    l_diagonals(board.reverse)
+  end
+
+  # True or false if there there are n tokens following each other,
+  # in any given selection of tokens (2d array of columns, rows diagonals of the board)
+  def n_consec_tokens?(token_collection)
+    token_collection.each do |token_row|
+      token = token_row[0]
+      token_count = 0
+      token_row.each do |cell_token|
+        if cell_token.nil?
+          token_count = 0
+          next
+        elsif cell_token == token
+          token_count += 1
+        else
+          token_count = 1
+          token = cell_token
+        end
+      end
+      return true if token_count == tokens_to_win
+    end
+    false
+  end
+
+  # Returns true  if any of the rows, columns, diagonals contain
+  # n consecutive tokens
+  def victory?
+    rows = board
+    if n_consec_tokens?(rows) ||
+       n_consec_tokens?(columns)     ||
+       n_consec_tokens?(l_diagonals) ||
+       n_consec_tokens?(r_diagonals)
+      true
+    end
+  end
+end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,80 +5,80 @@ RSpec.describe TicTacToe do
     board = [[1, 1, 1],
              [nil, nil, nil],
              [nil, nil, nil]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads horizontal victory II' do
     board = [[nil, nil, nil],
              [nil, nil, nil],
              [1, 1, 1]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads vertical victory I' do
     board = [[1, nil, nil],
              [1, nil, nil],
              [1, nil, nil]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads vertical victory II' do
     board = [[nil, nil, 1],
              [nil, nil, 1],
              [nil, nil, 1]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads diagonal victory I' do
     board = [[1, nil, nil],
              [nil, 1, nil],
              [nil, nil, 1]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads diagonal victory II' do
     board = [[nil, nil, 1],
              [nil, 1, nil],
              [1, nil, nil]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('victory')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('victory')
   end
   it 'reads tie' do
     board = [[0, 1, 1],
              [1, 0, 0],
              [1, 0, 1]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('tie')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('tie')
   end
   it 'reads tie II' do
     board = [[1, 1, 0],
              [0, 0, 1],
              [1, 1, 0]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('tie')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('tie')
   end
   it 'reads unfinished' do
     board = [[0, 1, nil],
              [1, nil, 1],
              [1, 1, 0]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('unfinished')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('unfinished')
   end
   it 'reads unfinished II' do
     board = [[0, 1, nil],
              [nil, nil, 1],
              [nil, 1, 0]]
-    game = GameManager.new
-    game.instance_variable_set(:@board, board)
-    expect(game.board_state?).to eq('unfinished')
+    gboard = Board.new
+    gboard.instance_variable_set(:@board, board)
+    expect(gboard.state?).to eq('unfinished')
   end
 end
