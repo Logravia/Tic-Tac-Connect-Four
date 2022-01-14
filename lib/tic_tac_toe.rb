a9ec9e4d56dc802bb6489cc98238e176000294e5
Logravia <j.alunans@gmail.com>
2022-01-14 16:43:22 +0200

    Add basic two player game loop

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,141 +4,57 @@ require 'pry-byebug'
 
 # Manages the game - main loop, asking players for input,
 # Asking BoardManager to place the input on its board
-# Asking DisplayManager to display board's data however it pleases
+# Asking CanvasManager to display board's data however it pleases
 class GameManager
-  # 2D array for 3x3 board
-  attr_reader :board_manager, :players, :screen
+  attr_reader :board_manager, :players, :canvas
 
-  def initialize(width, height)
-    @board_manager = BoardManager.new(width, height)
-    @screen = DisplayManager.new(width, height)
+  def initialize(width, height, tokens_to_win)
+    @board_manager = BoardManager.new(width, height, tokens_to_win)
+    @canvas = CanvasManager.new(width, height)
+    @players = [Player.new, Player.new]
   end
 
   def play
-    screen.update_canvas(board_manager.board)
-    screen.show_board
+    turn = 0
+    canvas.show
+    until board_manager.board_state? != 'unfinished'
+      which_player = turn % 2 # Depending on turn number it is either p0 or p1
+      succesful_choice = false
+
+      until succesful_choice
+        # TODO Make sure player can quit the game
+        choice = players[which_player].choose # Chooses a square to put his token on
+        # set_token returns bool on whether the choice was legitmate
+        succesful_choice = board_manager.set_token(which_player, choice)
+        puts 'Sorry, that is not a valid choice. Try again' unless succesful_choice
+      end
+      canvas.update(board_manager.board)
+      canvas.show
+      turn += 1
+    end
+    if board_manager.board_state? == 'victory'
+      puts "Player #{which_player} won!"
+    else
+      puts 'Game ended in a tie'
+    end
   end
 
   def reset
-    @board = Board.new
-  end
-end
-
-# Manages how the data of the board ought to be displayed
-# Generates an empty ASCII board of width*height squares
-# Puts on the ASCII board appropriate symbols depending on the state of the board
-# Prints the ASCII board with appropriate symbols onto the console
-class DisplayManager
-  attr_reader :circle, :cross, :canvas
-
-  def initialize(width, height)
-    @circle = { [1, 4] => '0', [2, 3] => '0', [2, 6] => '0', [3, 5] => '0' }
-    @cross = { [1, 2] => '#', [1, 6] => '#', [2, 4] => '#', [3, 2] => '#', [3, 6] => '#' }
-    @canvas =  gen_canvas(width, height)
-  end
-
-  def gen_canvas(width, height)
-    slate = []
-    height.times do
-      gen_horizontal_line(width, slate)
-      gen_vertical_line(width, slate)
-    end
-    gen_horizontal_line(width, slate)
-    slate
-  end
-
-  def gen_vertical_line(width, slate)
-    height = 3
-    spacing = 8
-    height.times do
-      line = []
-      width.times do
-        line << '|'
-        spacing.times do
-          line << ' '
-        end
-      end
-      # ending
-      line << '|'
-      line << "\n"
-      slate << line
-    end
-  end
-
-  def gen_horizontal_line(width, slate)
-    spacing = 8
-    line = []
-    width.times do
-      line << '+'
-      spacing.times do
-        line << '-'
-      end
-    end
-    line << '+'
-    line << "\n"
-    slate << line
-  end
-
-  def show_board
-    canvas.each do |row|
-      row.each do |val|
-        print val
-      end
-    end
-  end
-
-  def put_symbol(symbol, x, y)
-    y_offset = 4 * y
-    x_offset = 8 * x
-    symbol.each do |k, v|
-      canvas[k[0] + y_offset][k[1] + x_offset] = v
-    end
-  end
-
-  def update_canvas(board_state)
-    board_state.each_with_index do |row, nth_row|
-      row.each_with_index do |val, nth_column|
-        case val
-        when '1'
-          put_symbol(circle, nth_row, nth_column)
-        when '2'
-          put_symbol(cross, nth_row, nth_column)
-        end
-      end
-    end
-  end
-
-  def print_prompt; end
-  def print_result; end
-  def clear; end
-end
-
-# Manages players, e.g. gets input from them, stores their symbols and scores
-class Player
-  attr_accessor :symbol, :score, :name
-
-  def initialize(name, _ord)
-    @order = order
-    @name = name
-    @score = 0
-  end
-
-  def choose
-    chosen_square = gets.to_i
+    # TODO: implement the two functions
+    board_manager.reset
+    canvas.reset
   end
 end
 
 # Holds data about board of the game - where each player has put their piece.
 # Allows a piece to be put on the board.
-# Answers whether the game has been won, whether there's a tie or game can be continued.
-# Contains all the logic and methods to fulfill this purpose
+# Answers whether the game has been won, whether there's a tie or the game is unfinished
+# Contains all the logic and methods necessery to fulfill these three actions
 class BoardManager
   attr_reader :board, :size
 
   def initialize(width, height, tokens_to_win = 3)
     @board = Array.new(height) { Array.new(width) }
-    @board_to_draw = []
-    @size = size
     @tokens_to_win = tokens_to_win
   end
 
@@ -163,7 +79,8 @@ class BoardManager
   end
 
   # Returns three possible states of the board: tie, unfinished, victory
-  def state?
+  def board_state?
+    # TODO break-up this function to into victory?, finished?, tie? for better readability
     if victory?
       'victory'
     elsif board.flatten.any?(nil)
@@ -254,5 +171,115 @@ class BoardManager
   end
 end
 
-game = GameManager.new(3,3)
+# Manages how the data of the board ought to be displayed
+# Generates an empty ASCII board of width*height squares
+# Puts on the ASCII board appropriate symbols depending on the state of the board
+# Prints the ASCII board with appropriate symbols onto the console
+class CanvasManager
+  def initialize(width, height)
+    @circle = { [1, 4] => '0', [2, 3] => '0', [2, 6] => '0', [3, 5] => '0' }
+    @cross = { [1, 2] => '#', [1, 6] => '#', [2, 4] => '#', [3, 2] => '#', [3, 6] => '#' }
+    @canvas =  gen_canvas(width, height)
+  end
+
+  # Updates the ASCII canvas
+  # Puts symbols (cross,circle)
+  # In all the ASCCII squares where p1 or p2 has put their mark on the board
+  def update(board)
+    board.each_with_index do |row, nth_row|
+      row.each_with_index do |val, nth_column|
+        case val
+        when 0
+          put_symbol(circle, nth_row, nth_column)
+        when 1
+          put_symbol(cross, nth_row, nth_column)
+        end
+      end
+    end
+  end
+
+  def show
+    canvas.each do |row|
+      row.each do |val|
+        print val
+      end
+    end
+  end
+
+  private
+
+  attr_reader :circle, :cross, :canvas
+
+  # TODO Put a number at the edge of each square
+  def numer_squares
+    # Code
+  end
+
+  # Puts given symbol (cross, circle) onto the ASCII canvas contained in an array
+  def put_symbol(symbol, x, y)
+    # TODO fix spacing issues
+    y_offset = 4 * y
+    x_offset = 8 * x
+    symbol.each do |k, v|
+      canvas[k[0] + y_offset][k[1] + x_offset] = v
+    end
+  end
+
+
+  # Generates the ASCII board array with the help of gen_vertical_lines and gen_horizontal line
+  def gen_canvas(width, height)
+    slate = []
+    height.times do
+      gen_horizontal_lines(width, slate)
+      gen_vertical_lines(width, slate)
+    end
+    gen_horizontal_lines(width, slate)
+    slate
+  end
+
+  # Generates the vertical parts of an ASCII squares
+  def gen_vertical_lines(width, slate)
+    height = 3
+    spacing = 8
+    height.times do
+      line = []
+      width.times do
+        line << '|'
+        spacing.times do
+          line << ' '
+        end
+      end
+      # ending
+      line << '|'
+      line << "\n"
+      slate << line
+    end
+  end
+
+  # Generates the horizontal parts of the ASCII squares
+  def gen_horizontal_lines(width, slate)
+    spacing = 8
+    line = []
+    width.times do
+      line << '+'
+      spacing.times do
+        line << '-'
+      end
+    end
+    line << '+'
+    line << "\n"
+    slate << line
+  end
+end
+
+# Manages player, e.g. gets input from player,
+class Player
+  def initialize; end
+  def choose
+    # TODO make sure only numbers can be chosen
+    gets.to_i
+  end
+end
+
+game = GameManager.new(3, 3, 6)
 game.play
